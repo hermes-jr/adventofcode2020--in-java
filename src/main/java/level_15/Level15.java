13ae810325f087618d202c8f79a7f0f26ee75b50
Mikhail Antonov <hermes@cyllene.net>
2020-12-15 17:00:53 +0300

    Level 15 speedup

--- src/main/java/level_15/Level15.java
+++ src/main/java/level_15/Level15.java
@@ -1,40 +1,42 @@
 package level_15;
 
 import common.Level;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Level15 extends Level {
     static final boolean VERBOSE = false;
+    int[] seenA = new int[30_000_001];
+    int[] seenB = new int[30_000_001];
 
     int p1(String seed, int steps) {
-        Map<Integer, ImmutablePair<Integer, Integer>> memory = new HashMap<>();
+        Arrays.fill(seenA, -1);
         List<Integer> seeds = Arrays.stream(seed.split(",")).map(Integer::parseInt).collect(Collectors.toList());
         int lastSpoken = -1;
         for (int turn = 1; turn <= steps; turn++) {
             if (turn <= seeds.size()) {
-                lastSpoken = sayNumber(memory, seeds.get(turn - 1), turn);
+                lastSpoken = sayNumber(seeds.get(turn - 1), turn);
             } else {
-                if (VERBOSE) System.out.println(memory);
-                if (!memory.containsKey(lastSpoken)) {
+                int v = seenB[lastSpoken];
+                if (v == 0) {
                     lastSpoken = 0;
                 } else {
-                    ImmutablePair<Integer, Integer> seenTimes = memory.get(lastSpoken);
-                    lastSpoken = sayNumber(memory, seenTimes.left - seenTimes.right, turn);
+                    lastSpoken = sayNumber(seenA[lastSpoken] - seenB[lastSpoken], turn);
                 }
             }
         }
         return lastSpoken;
     }
 
-    private int sayNumber(Map<Integer, ImmutablePair<Integer, Integer>> memory, int number, int turn) {
+    private int sayNumber(int number, int turn) {
         if (VERBOSE) System.out.printf("turn %4d, num: %d%n", turn, number);
-        memory.put(number, ImmutablePair.of(turn, memory.getOrDefault(number, ImmutablePair.of(turn, turn)).left));
+        if (seenA[number] == -1) {
+            seenA[number] = turn;
+        }
+        seenB[number] = seenA[number];
+        seenA[number] = turn;
         return number;
     }
 
--- src/test/java/level_15/Level15Test.java
+++ src/test/java/level_15/Level15Test.java
@@ -1,5 +1,7 @@
 package level_15;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,10 +19,30 @@ class Level15Test {
             "3,2,1;438",
             "3,1,2;1836"
     }, delimiter = ';')
-    void partTwoExamplesShouldWork(String starting, int result) {
+    void partOneExamplesShouldWork(String starting, int result) {
         Level15 l = new Level15();
-        assertThat(l.p1(starting)).isEqualTo(result);
+        assertThat(l.p1(starting, 2020)).isEqualTo(result);
     }
 
+    @Test
+    void partOneFastExample() {
+        Level15 l = new Level15();
+        assertThat(l.p1("0,3,6", 2020)).isEqualTo(436);
+    }
+
+    @ParameterizedTest(name = "Part two examples should work")
+    @CsvSource(value = {
+            "0,3,6;175594",
+            "1,3,2;2578",
+            "2,1,3;3544142",
+            "1,2,3;261214",
+            "2,3,1;6895259",
+            "3,2,1;18",
+            "3,1,2;362",
+    }, delimiter = ';')
+    void partTwoExamplesShouldWork(String starting, int result) {
+        Level15 l = new Level15();
+        assertThat(l.p1(starting, 30_000_000)).isEqualTo(result);
+    }
 
 }
